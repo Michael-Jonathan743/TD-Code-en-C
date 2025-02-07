91679e6b766cfd91fa7bfe5bfe113f1078072fe2
Michael-Jonathan743 <randrianarisoamichaeljo@gmail.com>
2025-02-07 19:56:03 +0300

    exo9.c

---
+++ exo9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,i,p;
+    printf("Saisir un nombre: ");
+    scanf("%d",&a);
+    for(i=2;i<=a;i++)
+    {
+        p=i%2;
+        if(p==0)
+        {
+            printf(" %d",i);
+        }
+    }
+    return (0);
+}
