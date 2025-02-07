cf75f51a1359b0957b3116dcb5ed168224043c0f
Michael-Jonathan743 <randrianarisoamichaeljo@gmail.com>
2025-02-07 19:56:37 +0300

    exo10.c

---
+++ exo10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n,i,d;
+    printf("Saisir un nombre: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        d=n%i;
+        if(d==0)
+        {
+            printf(" %d",i);
+        }
+    }
+    return (0);
+}
