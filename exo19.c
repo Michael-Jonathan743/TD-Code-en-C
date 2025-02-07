80b14755c6fff399ec166a8516bc3924d9eb8fd5
Michael-Jonathan743 <randrianarisoamichaeljo@gmail.com>
2025-02-07 20:05:39 +0300

    exo19.c

---
+++ exo19.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,div;
+    printf("Veuillez saisir deux entiers naturels non nul.\na: ");
+    scanf("%d",&a);
+    printf("b: ");
+    scanf("%d",&b);
+    do 
+    {
+        div++;
+        if(a%div==0 && b%div==0)
+        {
+            c=div;
+        }
+    }while(div<=a && div<=b);
+    printf("PGCD(%d,%d)=%d\n",a,b,c);
+    if(c==1)
+    {
+        printf("%d et %d sont premiers entre eux.",a,b);
+    }
+    else
+    {
+       printf("%d et %d ne sont pas premiers entre eux.",a,b);
+    }
+    return 0;
+}
